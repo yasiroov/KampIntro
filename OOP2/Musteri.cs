338824c52136b2d1d55fe7e0fec522ea81144ed1
Yasirov <Yasirov@DESKTOP-AIIBM1H>
2021-01-23 23:52:11 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+        //Gerçek - Tüzel
+        // Eğer bir nesnede bir değeri kullanmak zorunda gibi görünmüyorsa soyutlama hatası yapılıyor.
+        //SO-L-ID
+    }
+}
---
+++ OOP3/IKrediManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+
+    // birbirinin alternatifi olan ama kod içerikleri farklı olan durumlar için kullanılır *interface
+    interface IKrediManager
+    {
+        void Hesapla();
+        void BirseyYap();
+        
+    }
+}
